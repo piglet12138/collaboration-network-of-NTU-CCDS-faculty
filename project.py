e79ac1a9ff9473e15cb24a9ef3166365eb8660d8
tuanla074 <32454569+tuanla074@users.noreply.github.com>
2025-04-18 03:28:07 -0400

    Update project.py

--- project.py
+++ project.py
@@ -16,7 +16,7 @@ output_json = r"papers_by_key_cleaned.json"
 
 process_faculty_folder(folder_path, output_json_path)
 deduplicate_paper_data(input_json, output_json)
-pageRankNetWork()
-new1000Network()
+page_rank_netWork()
+new_1000_network()
 overallInfo()
 network_degree_plot()
