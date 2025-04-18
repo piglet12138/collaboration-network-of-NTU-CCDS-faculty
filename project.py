bcc640eefa082cc99009e6155bb4b94d85dafde4
giangson19 <giangson.lumos@gmail.com>
2025-04-18 17:55:57 +0800

    add functions to analyze question 1 to 5 to project.py

--- project.py
+++ project.py
@@ -9,6 +9,16 @@ from faculty import *
 # generate_raw_data()
 # generate_network_links()
 
+# Run this to get statistics for Question 1
+get_network_statistics(year=2025)
+compare_with_random_network(year=2025)
+
+# Run this to get visualization for Question 2
+visualize_statistics_change()
+
+# Run this and change the attribute name to either "position", "management" or "area" to get the visualization for Question 3-5
+analyze_and_visualize_collaboration(year = 2025, attribute_name="position", min_faculty=3)
+
 #1000 preprocessing:
 folder_path = r"faculty_data" #subsitute with relative path
 output_json_path = r"papers_by_key.json" #relative path
