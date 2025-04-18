1d43a61a55869a93c6a78d1760568ac18ce6e103
giangson19 <giangson.lumos@gmail.com>
2025-04-18 20:17:52 +0800

    refactor comment and code in project.py

--- project.py
+++ project.py
@@ -8,27 +8,24 @@ from faculty import *
 # generate_raw_data()
 # generate_network_links()
 
-#run this for network visualization
-# networks = build_collaboration_networks('main_authors_collaborations.csv')
-# print_network_info(networks)
-# analyze_specific_year(networks, 2020)
-# visualize_year_network(networks, 2025)
-# visualize_network_evolution(networks, [2009, 2010, 2011])
-# visualize_years_network_grid(networks, 2001, 2025)
+### This should always be run to build the collaboration networks
+networks = build_collaboration_networks('main_authors_collaborations.csv')
 
-# Run this to get statistics for Question 1
+### Run this to get statistics for Question 2.1
+visualize_year_network(networks, 2025)
 get_network_statistics(year=2025)
 compare_with_random_network(year=2025)
 
-# Run this to get visualization for Question 2
+### Run this to get visualization for Question 2.2
+visualize_years_network_grid(networks, 2001, 2025)
 visualize_statistics_change()
 
-# Run this and change the attribute name to either "position", "management" or "area" to get the visualization for Question 3-5
+### Run this and change the attribute name to either "position", "management" or "area" to get the visualization for Question 2.3-2.5
 analyze_and_visualize_collaboration(year = 2025, attribute_name="position", min_faculty=3)
 analyze_and_visualize_collaboration(year = 2025, attribute_name="management", min_faculty=3)
 analyze_and_visualize_collaboration(year = 2025, attribute_name="area", min_faculty=3)
 
-#1000 preprocessing:
+### Run this to get new faculty data and analyze for section 4:
 folder_path = r"faculty_data" #subsitute with relative path
 output_json_path = r"papers_by_key.json" #relative path
 input_json = r"papers_by_key.json"
